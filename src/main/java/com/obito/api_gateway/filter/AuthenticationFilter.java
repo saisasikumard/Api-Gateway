dcebec3d3a07e6d95125e15e047beb50faa03192
sai <dhavileswarapusaikumar@gmail.com>
2025-02-24 12:57:58 +0530

    added Jwt Security

--- src/main/java/com/obito/api_gateway/filter/AuthenticationFilter.java
+++ src/main/java/com/obito/api_gateway/filter/AuthenticationFilter.java
@@ -20,31 +20,31 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
 
     @Override
     public GatewayFilter apply(Config config) {
-        return ((exchane,chain)->{
-            if(validator.isSecured.test(exchane.getRequest())){
+        return ((exchane,chain)-> {
+            if (validator.isSecured.test(exchane.getRequest())) {
                 //check if header is present or not.
-                if(!exchane.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                if (!exchane.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                     throw new RuntimeException("missing Authentiction Header");
                 }
-                }
-            String authHeaders=exchane.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-              if(authHeaders!=null && authHeaders.startsWith("Bearer "))
-            {
-                authHeaders=authHeaders.substring(7);
-                System.out.println(""+authHeaders);
+
+            System.out.println("Hi");
+            String authHeaders = exchane.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            if (authHeaders != null && authHeaders.startsWith("Bearer ")) {
+                authHeaders = authHeaders.substring(7);
+                System.out.println("" + authHeaders);
             }
-            try{
+            try {
                 System.out.println("Hi");
-                restTemplate.getForObject("http://localhost:9044/user/validatetoken?token="+authHeaders, String.class);
-            }
-            catch (Exception e){
+                // restTemplate.getForObject("http://localhost:9044/user/validatetoken?token="+authHeaders, String.class);
+            } catch (Exception e) {
                 System.out.println("Invalid token ....");
                 throw new RuntimeException("Invalid Token");
             }
+
+        }
             return chain.filter(exchane);
         });
     }
-
     public static class Config{
 
     }
